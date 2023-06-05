d40a0cf964b66a4e36e9fd49f8e8a3a91b17da7f
MrBurnsCFT <cristiantaborda@hotmail.com>
2023-06-05 15:05:11 -0300

    # commit - login

--- proyecto_ecommerce/administracion/urls.py
+++ proyecto_ecommerce/administracion/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_administracion, name='inicio_administracion'),
+    path('', views.index_administracion, name='inicio'),
+    path('administracion/signin/', views.signin, name='ingresar'),
+    path('administracion/signup/', views.signup, name='registrarse'),
+    path('administracion/signout/', views.signout, name='logout'),
 ]
--- proyecto_ecommerce/administracion/views.py
+++ proyecto_ecommerce/administracion/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index_administracion(request):
     variable='test variable'
     return render (request, 'administracion/index_administracion.html',{'variable':variable})
+
+def base(request):
+    return render(request, 'administracion/base.html')
+
+def signin(request):
+    return render(request, 'administracion/signin.html', {
+        'form': AuthenticationForm,
+        'error': 'Username and Password do not match'
+    })
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'administracion/signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], 
+                        password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return render(request, 'administracion/singup.html', {
+                'form': UserCreationForm,
+                'error': 'Username already exists'
+            })
+        return render(request, 'administracion/singup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match'
+        })
+                
+
+def signout(request):
+    return render(request, 'signout.html')
